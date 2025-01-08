357bb4c52eec7bce9da72d3f5b5961837a31ff43
Elmira13 <elmirakececi10@gmail.com>
2025-01-08 21:03:47 +0100

    changes in ex01

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 #include "ClapTrap.hpp"
 
@@ -8,28 +7,22 @@ int main()
     std::cout << "=== Default Constructor Test ===" << std::endl;
     ClapTrap clapDefault;
     ScavTrap scavDefault;
-    FragTrap fragDefault;
 
     std::cout << "\n=== Parameterized Constructor Test ===" << std::endl;
     ClapTrap clapParam("Clappy");
     ScavTrap scavParam("Scavvy");
-    FragTrap fragParam("Fraggy");
 
     std::cout << "\n=== Copy Constructor Test ===" << std::endl;
     ClapTrap clapCopy(clapParam);
     ScavTrap scavCopy(scavParam);
-    FragTrap fragCopy(fragParam);
 
     std::cout << "\n=== Copy Assignment Test ===" << std::endl;
     ClapTrap clapAssigned;
     ScavTrap scavAssigned;
-    FragTrap fragAssigned;
     clapAssigned = clapParam;
     scavAssigned = scavParam;
-    fragAssigned = fragParam;
 
     std::cout << "\n=== Testing Special Functionalities ===" << std::endl;
-    fragParam.highFivesGuys();
     scavParam.guardGate();
 
     std::cout << "\n=== Destruction Test ===" << std::endl;
